5786427beb98b7d5b6689fc46c5a07f9449c03a0
ali aziz <aa_connect@outlook.com>
2018-07-14 10:52:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -12,7 +12,7 @@ def countdown_with_sleep(num_secs)
   while number > 0 do
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleep(5.seconds)
+    sleep 6
   end
   return "HAPPY NEW YEAR!"
 end
